74068bff3fa5271d8d837ff3ecad2a9ae609420c
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-10-30 09:26:58 +0100

    uml0

--- src/colons/Main.java
+++ src/colons/Main.java
@@ -4,4 +4,5 @@ package colons;
  * Created by val on 29/10/14.
  */
 public class Main {
+    protected java.util.ArrayList<Carte> cartes;
 }
--- src/colons/Ressource.java
+++ src/colons/Ressource.java
@@ -3,5 +3,5 @@ package colons;
 /**
  * Created by val on 29/10/14.
  */
-public class Ressource {
+public class Ressource extends Carte {
 }
