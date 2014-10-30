fe98fcfef20cf0df7fa6b0e1426eca328663ccc1
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-10-30 16:33:03 +0100

    uml2

--- src/colons/Joueur.java
+++ src/colons/Joueur.java
@@ -7,6 +7,7 @@ public class Joueur {
     protected Main main;
     protected int score;
     protected java.util.ArrayList<Uv> ListUv;
+    private java.util.ArrayList<ControleContinu> listCC;
 
     public void CalculerScore() {
     }
