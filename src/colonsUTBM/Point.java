dba74f56dec935b9485f2121f215e5e0db0cf78c
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-12-11 17:40:13 +0100

    Restauration manuelle de Point.java (GG Guigui)

--- src/colonsUTBM/Point.java
+++ src/colonsUTBM/Point.java
@@ -1,38 +1,66 @@
 package colonsUTBM;
-
+import java.text.DecimalFormat;
 /**
- * Created by Guillaume on 02/12/2014.
+ * Created by val on 26/11/14.
  */
 public class Point {
     protected double x;
     protected double y;
-
-    public Point() {
+    /**
+     * Constructeur par défaut, initialise un point de coordonnées (0,0).
+     */
+    public Point(){
+        this.x=0;
+        this.y=0;
     }
-
-    public Point(double _x, double _y) {
+    /**
+     * Constructeur par coordonnées, crée un point de coordonnées (x,y).
+     * @param x
+     * @param y
+     */
+    public Point(double x, double y){
+        this.x=x;
+        this.y=y;
     }
-
-    public Point(Point p) {
+    /**
+     * Constructeur par copie.
+     * @param p
+     */
+    public Point(Point p){
+        this.x=p.x;
+        this.y=p.y;
     }
-
-    public double getX(){
-        return x;
+    public double getX() {return x;}
+    public void setX(double x) {this.x = x;}
+    public double getY() {return y;}
+    public void setY(double y) {this.y = y;}
+    /**
+     * transformer coordonnées du repère "hexagonal" à orthonormé.
+     */
+    public void transformer(){
+        x=x-0.5*y;
+        y=y*2/Math.sqrt(3);
     }
-
-    public double getY(){
-        return y;
+    /**
+     * Retourne x sous repère orthonormé.
+     * @return
+     */
+    public double transformerX(){
+        return (x-0.5*y)*1.35;
     }
-
+    /**
+     *Retourne y sous repère orthonormé.
+     * @return
+     */
+    public double transformerY(){
+        return y*2/Math.sqrt(3);
+    }
+    /**
+     * Retourne une chaine des coordonnées du type i[x]j[y].
+     * @return
+     */
     public String getStringPointID(){
-        return "(" + x + ";" + y + ")";
-    }
-
-    public double transformerX() {
-        return 0;
-    }
-
-    public double transformerY() {
-        return 0;
+        DecimalFormat df = new DecimalFormat("#.##");
+        return "i"+df.format(x)+"j"+df.format(y);
     }
 }
