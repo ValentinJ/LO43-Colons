f0d95d0ace645a17b66611a441d77bb76561af90
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-12-12 21:46:46 +0100

    Finalisation classe Des

--- src/colonsUTBM/Des.java
+++ src/colonsUTBM/Des.java
@@ -1,21 +1,31 @@
 package colonsUTBM;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Guillaume on 02/12/2014.
  */
 public class Des {
-    public ArrayList<String> imgFace;
     public int v1;
     public int v2;
 
-    public Des(){v1=0; v2=0;}
+    public Des(){v1=1; v2=1;}
 
-    public ArrayList<Integer> affichage(int indice){
-        ArrayList<Integer> liste = new ArrayList<Integer>();
-        liste.add(v1);
-        liste.add(v2);
-        return liste;
+    public void lancerDes(){
+        Random r = new Random();
+        v1 = 1 + r.nextInt(6);
+        v2 = 1 + r.nextInt(6);
+    }
+
+    public int getTotalDes(){
+        return v1+v2;
+    }
+
+    public String getImgV1(){
+        return "des"+v1+".png";
+    }
+    public String getImgV2(){
+        return "des"+v2+".png";
     }
 }
