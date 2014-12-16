bf414b7baf51926fb55b7fddaad1942541c99840
felmann <felmann.guillaume@gmail.com>
2014-12-16 15:27:54 +0100

    juste cacher les erreurs

--- src/fenetreGraphique/Plateau.java
+++ src/fenetreGraphique/Plateau.java
@@ -9,23 +9,23 @@ import java.awt.*;
  * Created by Guillaume on 21/11/2014.
  */
 public class Plateau extends JPanel {
-    protected GraphMap graph;
+    public GraphMap graph;
 
     // initialisation du plateau de jeu
     public Plateau(GraphMap g){
-        setBackground(Color.cyan);
         graph= g;
 
         setLayout(new BorderLayout());
+        graph.chargerCSS();
+        graph.MAJCSS();
 
         add(graph.getView(),BorderLayout.CENTER);
         //add(new JLabel("ici devrait etre la map avec le res des dés"));
     }
 
     public void MiseAJour(){
-        graph.MAJCSS();
         graph.chargerCSS();
-
+        graph.MAJCSS();
     }
 
     // possible interraction
