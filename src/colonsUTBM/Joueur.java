9c4fe0183997e4bea98d376c90c19dd26a1e7a5a
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-12-16 18:12:11 +0100

    Classe Joueur : descriptif méthodes

--- src/colonsUTBM/Joueur.java
+++ src/colonsUTBM/Joueur.java
@@ -13,6 +13,67 @@ public class Joueur {
     protected int nb_UV2;
     protected int nb_CC;
 
+    // Méthodes boolean pour construction UV1, Uv2, Controle continu, Achat Carte developpement
+    /**
+     * Pour les construction uv1, UV2, Controle continue -> verifier ressource et nb de pions !=0
+     * Construction d'une carte de dev -> verifier ressource
+     */
+
+    // Méthodes construction avec params (GraphMap g)
+    /**
+     * vérifier avec la méthode vue plus haut
+     * retirer ressources
+     * retirer pions
+     * Ajouter l'uv dans l'arraylist des uv du joueur (voir la méthode récupérer uv du joueur)
+     * normalement rien d'autre à faire  À VÉRIFIER !!
+     */
+
+    //Méthode récupérer UV du joueur à partir du graph et les stocker dans arraylist du joueur
+
+    // Méthode pour faire commerce (argument un joueur si commerce avec joueur ou voir pour les autres...)
+
+    //Méthode savoir si joueur peut faire un type de commerce que ce soit commerce 3:1 , commerce banque, commerce 2:1 en fonction de la ressource (retourner un boolean)
+
+    //Joueur carteCC et carteAncien (arg graphmap g) (deux méthodes différentes !! )
+    /**
+     * CarteCC
+     * construire deux CC sans retirer ressource mais retirer pions
+     *
+     * Carte Ancien
+     * deplacer binome glandeur
+     * choisir joueur voisin au binome pour lui voler une carte au hasard (prendre méthode de grpahmap qui retourne arraylist des joueur voisins à un hexagone ici binome glandeur)
+     */
+
+    //Méthode Joueur carte Monopole (arg arraylist des joeuurs, Enum du type de la carte)
+    /**
+     * Pour chaque joueur j de l'arraylist
+     * Verifier que ce n'est pas le propre joueur
+     * retirer les carte de ce type de carte
+     * ajouter au joueur courant
+     */
+
+    //Méthode Jouer carte decouverte
+    /**
+     * Retirer 2 carte du meme type à la banque et l'ajouter au joueur
+     */
+
+    //Méthode enleve moitié des carte
+    /**
+     * Si plus de 7 cartes enlever la moitié aléatoirement arrondi à l'inférieur (donc si 9 carte alors enlever 4 cartes)
+     */
+
+    //Méthode déplacer binome glandeur (arg Graphmap g)
+    /**
+     * lancer la méthode adéquate de graphmap et voilà
+     */
+
+    //Méthode récupérer nombre cartes
+    /**
+     * à partir de la arraylist pile du joueur (n'est pas implémenté, à remplacer à la place de arralylist carteRessource)
+     * récupérer le nombre de carte, pour chaque pile recuperer nb dans la pile et faire la somme
+     */
+
+    
     protected ArrayList<UV1> uvs;
     protected ArrayList<ControleContinueold> CC;
 
@@ -21,6 +82,7 @@ public class Joueur {
 
     public Joueur(){
     }
+
 
     public Joueur(String _nom, TypeCouleur _couleur){
         nom = _nom;
